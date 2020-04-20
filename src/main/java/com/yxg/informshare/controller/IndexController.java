4ed2944c0fc1858b12a98d0ef3819dc433cf6657
yinxg <858948387@qq.com>
2020-04-20 22:21:32 +0800

    ..

--- src/main/java/com/yxg/informshare/controller/IndexController.java
+++ src/main/java/com/yxg/informshare/controller/IndexController.java
@@ -1,31 +1,25 @@
 package com.yxg.informshare.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.lang.annotation.Annotation;
 
 /**
  * @Description TODO
  * @Author xiguang_yin
- * @Date 2020/4/16 10:37
+ * @Date 2020/4/20 19:29
  */
-public class IndexController implements Controller {
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        ModelAndView mav = new ModelAndView("index.jsp");
-        mav.addObject("message", "Hello Spring MVC");
+@Controller
+public class IndexController {
+    @RequestMapping("/index")
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception{
+
+//        request.getRequestDispatcher("login.html").forward(request, response);;
+        ModelAndView mav = new ModelAndView("index");
+
         return mav;
-    }
-
-    @Override
-    public String value() {
-        return null;
-    }
-
-    @Override
-    public Class<? extends Annotation> annotationType() {
-        return null;
     }
 }
---
+++ src/main/java/com/yxg/informshare/controller/LoginRegisterController.java
@@ -0,0 +1,27 @@
+package com.yxg.informshare.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+
+/**
+ * @Description TODO
+ * @Author xiguang_yin
+ * @Date 2020/4/14 21:50
+ */
+@Controller
+public class LoginRegisterController {
+    @RequestMapping("/login_register")
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception{
+
+//        request.getRequestDispatcher("login.html").forward(request, response);;
+        ModelAndView mav = new ModelAndView("login_register");
+
+        return mav;
+    }
+
+}
